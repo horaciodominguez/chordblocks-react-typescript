88fe6fc75f54a82624a2449c6366b3c792fb7ac1
Horacio Dominguez <hord17@gmail.com>
2025-07-26 23:32:55 -0300

    React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
-/*import { useEffect, useState } from "react"
-import { type Song } from "./types/song"
-import { songsMock } from "./data/songsMock"
-import { Header } from "./components/Header"
+/*
 import { SongList } from "./components/SongList"
 import { SongForm } from "./components/SongForm"
-import { Footer } from "./components/Footer"
-
 
 function App() {
 
-  const [songs, setSongs] = useState<Song[]>([])
-  
-  useEffect(()=>{
-    setSongs(songsMock)
-  }, [])
-
   const handleAddSong = (song: Song) => {
-    const newSongs = 
-        [...songs,
-        song]
-    setSongs(newSongs)
+      const newSongs = 
+          [...songs,
+          song]
+      setSongs(newSongs)
   }
 
   return (
-    <main className="
-      max-w-3xl mx-auto px-4 py-8
-      flex flex-col justify-center align-middle 
-      min-h-screen"
-    >
-      <Header />
-      <SongList songs={songs} />
+    
+      
       <SongForm handleAddSong={handleAddSong} />
-      <Footer />
-    </main>
+      
   )
 }*/
 
@@ -41,17 +24,27 @@ import Home from "./pages/Home"
 import ViewSong from "./pages/ViewSong"
 import NewSong from "./pages/NewSong"
 import EditSong from "./pages/EditSong"
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/song/:id" element={<ViewSong />} />
-        <Route path="/song/:id/edit" element={<EditSong />} />
-        <Route path="/new" element={<NewSong />} />
-      </Routes>
-    </BrowserRouter>
+    <main className="
+      max-w-3xl mx-auto px-4 py-8
+      flex flex-col justify-center align-middle 
+      min-h-screen"
+    >
+      <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/song/:id" element={<ViewSong />} />
+            <Route path="/song/:id/edit" element={<EditSong />} />
+            <Route path="/new" element={<NewSong />} />
+          </Routes>
+        </BrowserRouter>
+      <Footer />
+    </main>
     )
 }
 
