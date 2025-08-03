86728b3ee8b0238114f112172e98068196288861
Horacio Dominguez <hord17@gmail.com>
2025-08-02 22:53:02 -0300

    Refactor

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -15,18 +15,47 @@ export const SongList: React.FC<Props> = ({songs}) => {
                         key={song.id} 
                          className="border-[.1px] border-gray-700 bg-gray-50/5 rounded-md p-4 shadow-sm"
                         >
-                    <h3 className="font-medium">{song.title}</h3>
-                    <p>{song.author}</p>
-                    {
-                    song.songSections.map (
-                        (block, blockIndex) => (
-                        <p 
-                        key={blockIndex}
-                        className="inline-block bg-blue-100 text-gray-800 px-2 py-1 rounded-full text-xs mr-2 mt-1"
-                        >{block}</p>
+                        <h3 className="font-medium">{song.title}</h3>
+                        <p>{song.author}</p>
+                        <ul>
+                        {
+                        song.songSections.map (
+                            (section, sectionIndex) => (
+
+                            <li
+                            key={sectionIndex}
+                            
+                            >
+                                <p className="inline-block bg-blue-100 text-gray-800 px-2 py-1 rounded-full text-xs mr-2 mt-1 mb-2">
+                                    {
+                                        section.type
+                                    }
+                                </p>
+                                <div className="flex flex-wrap justify-items-start mb-2">
+                                    {section.blocks.map(
+                                        block => (
+                                            block.chords.map(
+                                                (chord, i) => (
+                                                    
+                                                    <div
+                                                        key={i}
+                                                        className="bg-blue-950 flex items-center justify-center py-4 mb-2"
+                                                        style={{
+                                                            width: `${(chord.beats / (song.timeSignature.beatsPerMeasure * 4)) * 100}%` // 4 compases por bloque
+                                                        }}
+                                                        >
+                                                        {chord.name}
+                                                    </div>
+                                                )
+                                            )
+                                        )
+                                    )}
+                                </div>
+                            </li>
+                            )
                         )
-                    )
-                    }
+                        }
+                        </ul>
                     </li>
                 )
             )
