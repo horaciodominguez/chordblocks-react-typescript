775c325fe40cbc6c7f446ae76515a2d42fdc2939
Horacio Dominguez <hord17@gmail.com>
2025-08-09 13:22:34 -0300

    Componentization

---
+++ src/components/Song.tsx
@@ -0,0 +1,42 @@
+import type { Song as SongType } from "../types/song"
+import { Block } from "./Block"
+import { SectionTag } from "./SectionTag"
+
+type Props = {
+    song: SongType
+}
+
+export const Song = ({song}: Props) => {
+    return (
+        <div 
+            key={song.id} 
+                className="border-[.1px] border-gray-700 bg-gray-50/5 rounded-md p-4 shadow-sm"
+            >
+            <h3 className="font-medium">{song.title}</h3>
+            <p>{song.author}</p>
+            <ul>
+            {
+            song.songSections.map (
+                (section, sectionIndex) => (
+
+                <li key={sectionIndex} >
+                    <SectionTag typeName={section.type} />
+                    <div className="flex flex-wrap justify-items-start mb-2">
+                        {section.blocks.map(
+                            block => (
+                                block.chords.map(
+                                    (chord, i) => (
+                                        <Block key={i} song={song} chord={chord} />
+                                    )
+                                )
+                            )
+                        )}
+                    </div>
+                </li>
+                )
+            )
+            }
+            </ul>
+        </div>
+    )
+}
--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,62 +1,21 @@
 
-import { type Song } from "../types/song"
-import { SectionTag } from "./SectionTag"
+import { type Song as SongType } from "../types/song"
+import { Song } from "./Song"
 
 interface Props {
-  songs: Song[]
+  songs: SongType[]
 }
 
 export const SongList = ({songs}: Props) => {
     return (
-        <ul className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
             {
             songs.map (
-                song =>(
-                    <li 
-                        key={song.id} 
-                         className="border-[.1px] border-gray-700 bg-gray-50/5 rounded-md p-4 shadow-sm"
-                        >
-                        <h3 className="font-medium">{song.title}</h3>
-                        <p>{song.author}</p>
-                        <ul>
-                        {
-                        song.songSections.map (
-                            (section, sectionIndex) => (
-
-                            <li
-                            key={sectionIndex}
-                            
-                            >
-                                <SectionTag typeName={section.type} />
-                                <div className="flex flex-wrap justify-items-start mb-2">
-                                    {section.blocks.map(
-                                        block => (
-                                            block.chords.map(
-                                                (chord, i) => (
-                                                    
-                                                    <div
-                                                        key={i}
-                                                        className="bg-blue-950 flex items-center justify-center py-4 mb-2"
-                                                        style={{
-                                                            width: `${(chord.beats / (song.timeSignature.beatsPerMeasure * 4)) * 100}%` // 4 compases por bloque
-                                                        }}
-                                                        >
-                                                        {chord.name}
-                                                    </div>
-                                                )
-                                            )
-                                        )
-                                    )}
-                                </div>
-                            </li>
-                            )
-                        )
-                        }
-                        </ul>
-                    </li>
+                (song, i) =>(
+                    <Song key={i} song={song} />
                 )
             )
             }
-        </ul>
+        </div>
     )
 }
