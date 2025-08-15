a70549444ccd21d80de46889c13812c118da2c99
Horacio Dominguez <hord17@gmail.com>
2025-08-15 18:04:48 -0300

    key by song id

--- src/modules/songs/components/SongList.tsx
+++ src/modules/songs/components/SongList.tsx
@@ -10,15 +10,15 @@ export const SongList = ({songs}: Props) => {
     return (
         <div className="flex flex-col gap-4">
             {
-            songs.map (
-                (song, i) =>(
-                    <div key={i} className="border-[.1px] border-gray-700 bg-gray-50/5 rounded-md shadow-sm">
-                        <h2>
-                            <Link className="block px-8 py-4" to={`/song/${song.id}`}>{song.title}</Link>
-                        </h2>
-                    </div>
+                songs.map (
+                    song =>(
+                        <div key={song.id} className="border-[.1px] border-gray-700 bg-gray-50/5 rounded-md shadow-sm">
+                            <h2>
+                                <Link className="block px-8 py-4" to={`/song/${song.id}`}>{song.title}</Link>
+                            </h2>
+                        </div>
+                    )
                 )
-            )
             }
         </div>
     )
