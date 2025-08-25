af75298cb104ac70776c825baefb24c549314f80
Horacio Dominguez <hord17@gmail.com>
2025-08-25 02:36:38 -0300

    Format code in NewSong component for improved readability

--- src/pages/NewSong.tsx
+++ src/pages/NewSong.tsx
@@ -2,21 +2,21 @@ import { SongForm } from "@/modules/songs/components/SongForm"
 import type { Song } from "@/modules/songs/types/song.types"
 
 type Props = {
-    songs: Song[],
-    setSongs: React.Dispatch<React.SetStateAction<Song[]>>
+  songs: Song[],
+  setSongs: React.Dispatch<React.SetStateAction<Song[]>>
 }
 
 export default function NewSong ({ songs, setSongs }: Props) {
 
-    const handleAddSong = (song: Song) => {
-        const newSongs = [...songs,song]
-        setSongs(newSongs)
-    }
+  const handleAddSong = (song: Song) => {
+    const newSongs = [...songs, song]
+    setSongs(newSongs)
+  }
 
-    return (
-        <>
-            <h2>New Song</h2>
-            <SongForm handleAddSong={handleAddSong} />
-        </>
-    )
+  return (
+    <>
+      <h2>New Song</h2>
+      <SongForm handleAddSong={handleAddSong} />
+    </>
+  )
 }
