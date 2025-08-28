1a60e624ca785beaf47463f2642c8b660a29ca29
Horacio Dominguez <hord17@gmail.com>
2025-08-28 01:25:15 -0300

    Update BEAT_VALUES to include all values from 1 to 12

--- src/modules/songs/constants/song.ts
+++ src/modules/songs/constants/song.ts
@@ -8,6 +8,6 @@ export const SECTION_OPTIONS = [
   "OUTRO",
 ] as const
 
-export const BEAT_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const
+export const BEAT_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const
 
 export const noteValues = [2, 4, 8] as const
