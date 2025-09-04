4d22071173ae433d03e023a9fe056b0ed7d8728f
Horacio Dominguez <hord17@gmail.com>
2025-09-03 23:24:21 -0300

    Refactor chords data structure for improved maintainability and clarity

--- src/modules/chords/data/chords.ts
+++ src/modules/chords/data/chords.ts
@@ -1,124 +1,39 @@
 import { type Chord } from "../types/chord.types"
 
-export const chordsData: Record<string, Chord[]> = {
-  C: [
-    { name: "C", type: "major" },
-    { name: "Cm", type: "minor" },
-    { name: "C7", type: "dominant7" },
-    { name: "Cm7", type: "minor7" },
-    { name: "Cmaj7", type: "major7" },
-    { name: "C9", type: "dominant9" },
-    { name: "Csus2", type: "sus2" },
-    { name: "Csus4", type: "sus4" },
-  ],
-  "C#": [
-    { name: "C#", type: "major" },
-    { name: "C#m", type: "minor" },
-    { name: "C#7", type: "dominant7" },
-    { name: "C#m7", type: "minor7" },
-    { name: "C#maj7", type: "major7" },
-    { name: "C#9", type: "dominant9" },
-    { name: "C#sus2", type: "sus2" },
-    { name: "C#sus4", type: "sus4" },
-  ],
-  D: [
-    { name: "D", type: "major" },
-    { name: "Dm", type: "minor" },
-    { name: "D7", type: "dominant7" },
-    { name: "Dm7", type: "minor7" },
-    { name: "Dmaj7", type: "major7" },
-    { name: "D9", type: "dominant9" },
-    { name: "Dsus2", type: "sus2" },
-    { name: "Dsus4", type: "sus4" },
-  ],
-  "D#": [
-    { name: "D#", type: "major" },
-    { name: "D#m", type: "minor" },
-    { name: "D#7", type: "dominant7" },
-    { name: "D#m7", type: "minor7" },
-    { name: "D#maj7", type: "major7" },
-    { name: "D#9", type: "dominant9" },
-    { name: "D#sus2", type: "sus2" },
-    { name: "D#sus4", type: "sus4" },
-  ],
-  E: [
-    { name: "E", type: "major" },
-    { name: "Em", type: "minor" },
-    { name: "E7", type: "dominant7" },
-    { name: "Em7", type: "minor7" },
-    { name: "Emaj7", type: "major7" },
-    { name: "E9", type: "dominant9" },
-    { name: "Esus2", type: "sus2" },
-    { name: "Esus4", type: "sus4" },
-  ],
-  F: [
-    { name: "F", type: "major" },
-    { name: "Fm", type: "minor" },
-    { name: "F7", type: "dominant7" },
-    { name: "Fm7", type: "minor7" },
-    { name: "Fmaj7", type: "major7" },
-    { name: "F9", type: "dominant9" },
-    { name: "Fsus2", type: "sus2" },
-    { name: "Fsus4", type: "sus4" },
-  ],
-  "F#": [
-    { name: "F#", type: "major" },
-    { name: "F#m", type: "minor" },
-    { name: "F#7", type: "dominant7" },
-    { name: "F#m7", type: "minor7" },
-    { name: "F#maj7", type: "major7" },
-    { name: "F#9", type: "dominant9" },
-    { name: "F#sus2", type: "sus2" },
-    { name: "F#sus4", type: "sus4" },
-  ],
-  G: [
-    { name: "G", type: "major" },
-    { name: "Gm", type: "minor" },
-    { name: "G7", type: "dominant7" },
-    { name: "Gm7", type: "minor7" },
-    { name: "Gmaj7", type: "major7" },
-    { name: "G9", type: "dominant9" },
-    { name: "Gsus2", type: "sus2" },
-    { name: "Gsus4", type: "sus4" },
-  ],
-  "G#": [
-    { name: "G#", type: "major" },
-    { name: "G#m", type: "minor" },
-    { name: "G#7", type: "dominant7" },
-    { name: "G#m7", type: "minor7" },
-    { name: "G#maj7", type: "major7" },
-    { name: "G#9", type: "dominant9" },
-    { name: "G#sus2", type: "sus2" },
-    { name: "G#sus4", type: "sus4" },
-  ],
-  A: [
-    { name: "A", type: "major" },
-    { name: "Am", type: "minor" },
-    { name: "A7", type: "dominant7" },
-    { name: "Am7", type: "minor7" },
-    { name: "Amaj7", type: "major7" },
-    { name: "A9", type: "dominant9" },
-    { name: "Asus2", type: "sus2" },
-    { name: "Asus4", type: "sus4" },
-  ],
-  "A#": [
-    { name: "A#", type: "major" },
-    { name: "A#m", type: "minor" },
-    { name: "A#7", type: "dominant7" },
-    { name: "A#m7", type: "minor7" },
-    { name: "A#maj7", type: "major7" },
-    { name: "A#9", type: "dominant9" },
-    { name: "A#sus2", type: "sus2" },
-    { name: "A#sus4", type: "sus4" },
-  ],
-  B: [
-    { name: "B", type: "major" },
-    { name: "Bm", type: "minor" },
-    { name: "B7", type: "dominant7" },
-    { name: "Bm7", type: "minor7" },
-    { name: "Bmaj7", type: "major7" },
-    { name: "B9", type: "dominant9" },
-    { name: "Bsus2", type: "sus2" },
-    { name: "Bsus4", type: "sus4" },
-  ],
-}
+// chords.ts
+export const ROOTS = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+] as const
+
+export const VARIATIONS = [
+  { suffix: "", type: "major" },
+  /* { suffix: "m", type: "minor" },
+  { suffix: "7", type: "dominant7" },
+  { suffix: "m7", type: "minor7" },
+  { suffix: "maj7", type: "major7" },
+  { suffix: "9", type: "dominant9" },
+  { suffix: "sus2", type: "sus2" },
+  { suffix: "sus4", type: "sus4" }, */
+] as const
+
+// Genera todas las combinaciones raíz + variación
+export const chordsData = ROOTS.reduce<Record<string, Chord[]>>((acc, root) => {
+  acc[root] = VARIATIONS.map((v) => ({
+    name: `${root}${v.suffix}`,
+    root,
+    type: v.type,
+    svgId: `${root}${v.suffix}`, // id en el sprite SVG
+  }))
+  return acc
+}, {})
