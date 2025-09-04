d991ceb468d902e41ff44153f1e3869bb2ed32e8
Horacio Dominguez <hord17@gmail.com>
2025-09-04 20:22:00 -0300

    refactor: clean up chords.ts by removing commented code and unnecessary comments

--- src/modules/chords/data/chords.ts
+++ src/modules/chords/data/chords.ts
@@ -1,6 +1,5 @@
 import { type Chord } from "../types/chord.types"
 
-// chords.ts
 export const ROOTS = [
   "C",
   "C#",
@@ -19,21 +18,19 @@ export const ROOTS = [
 export const VARIATIONS = [
   { suffix: "", type: "major" },
   { suffix: "m", type: "minor" },
-  /* { suffix: "7", type: "dominant7" },
+  { suffix: "7", type: "dominant7" },
   { suffix: "m7", type: "minor7" },
   { suffix: "maj7", type: "major7" },
   { suffix: "9", type: "dominant9" },
   { suffix: "sus2", type: "sus2" },
-  { suffix: "sus4", type: "sus4" }, */
+  { suffix: "sus4", type: "sus4" },
 ] as const
 
-// Genera todas las combinaciones raíz + variación
 export const chordsData = ROOTS.reduce<Record<string, Chord[]>>((acc, root) => {
   acc[root] = VARIATIONS.map((v) => ({
     name: `${root}${v.suffix}`,
     root,
     type: v.type,
-    svgId: `${root}${v.suffix}`, // id en el sprite SVG
   }))
   return acc
 }, {})
