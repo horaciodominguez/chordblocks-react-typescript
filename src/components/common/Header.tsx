7ca8c3cb2942297b2edb1d00989d8d75b645d3ff
Horacio Dominguez <hord17@gmail.com>
2025-09-11 19:48:13 -0300

    update Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 import { ListMusic, ListPlus } from "lucide-react"
 
@@ -11,26 +11,34 @@ export function Header() {
       <nav className="my-4">
         <ul className="flex gap-2">
           <li className="border-[.1px] bg-zinc-100/5 border-blue-900 rounded-md shadow-md hover:bg-zinc-100/10 hover:border-blue-600 transition-all">
-            <Link
+            <NavLink
               to="/"
-              className="flex flex-col justify-center items-center py-2 px-4 "
+              className={({ isActive }) =>
+                `flex flex-col justify-center items-center py-2 px-4 ${
+                  isActive ? "bg-zinc-100/10" : ""
+                }`
+              }
             >
               <ListMusic className="text-zinc-200 mb-2" />
               <span className="text-zinc-200 text-xs font-bold uppercase">
                 Song List
               </span>
-            </Link>
+            </NavLink>
           </li>
           <li className="border-[.1px] bg-zinc-100/5 border-blue-900 rounded-md shadow-md hover:bg-zinc-100/10 hover:border-blue-600 transition-all">
-            <Link
+            <NavLink
               to="/new"
-              className="flex flex-col justify-center items-center py-2 px-4"
+              className={({ isActive }) =>
+                `flex flex-col justify-center items-center py-2 px-4 ${
+                  isActive ? "bg-zinc-100/10" : ""
+                }`
+              }
             >
               <ListPlus className="text-zinc-200 mb-2" />
               <span className="text-zinc-200 text-xs font-bold uppercase">
                 Add Song
               </span>
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </nav>
