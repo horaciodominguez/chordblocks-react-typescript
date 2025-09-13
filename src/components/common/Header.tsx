b30b2f1c2e952deadd4d4d00065ac20565a082e7
Horacio Dominguez <hord17@gmail.com>
2025-09-13 00:15:18 -0300

    feat: update Header component to include logo and improve navigation styling

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,36 +6,64 @@ export function Header() {
   return (
     <>
       <h1 className="text-gray-200 text-3xl uppercase text-center p-6">
-        SongBlocks
+        <figure>
+          <img
+            src="./assets/logo.svg"
+            alt="ChordBlocks logo"
+            className="mx-auto"
+            width={200}
+          />
+        </figure>
       </h1>
       <nav className="my-4">
         <ul className="flex gap-2">
-          <li className="border-[.1px] bg-zinc-100/5 border-blue-900 rounded-md shadow-md hover:bg-zinc-100/10 hover:border-blue-600 transition-all">
+          <li
+            className="
+              border-[.1px] 
+              border-indigo-200/10
+              rounded-md
+              bg-indigo-200/5
+              shadow-lg 
+              hover:bg-indigo-600/20
+              duration-300
+              transition-all"
+          >
             <NavLink
               to="/"
               className={({ isActive }) =>
-                `flex flex-col justify-center items-center py-2 px-4 ${
-                  isActive ? "bg-zinc-100/10" : ""
+                `flex flex-col rounded-md justify-center items-center py-2 px-4 ${
+                  isActive ? "border-1 border-indigo-700" : ""
                 }`
               }
             >
-              <ListMusic className="text-zinc-200 mb-2" />
-              <span className="text-zinc-200 text-xs font-bold uppercase">
+              <ListMusic className="text-indigo-700 mb-2" />
+              <span className="text-indigo-700 text-xs font-bold uppercase">
                 Song List
               </span>
             </NavLink>
           </li>
-          <li className="border-[.1px] bg-zinc-100/5 border-blue-900 rounded-md shadow-md hover:bg-zinc-100/10 hover:border-blue-600 transition-all">
+          <li
+            className="
+            border-[.1px] 
+            border-indigo-200/10
+            rounded-md
+            bg-indigo-200/5
+            shadow-lg 
+            hover:bg-indigo-600/20
+            duration-300
+            transition-all
+          "
+          >
             <NavLink
               to="/new"
               className={({ isActive }) =>
-                `flex flex-col justify-center items-center py-2 px-4 ${
-                  isActive ? "bg-zinc-100/10" : ""
+                `flex flex-col rounded-md justify-center items-center py-2 px-4 ${
+                  isActive ? "border-1 border-indigo-700" : ""
                 }`
               }
             >
-              <ListPlus className="text-zinc-200 mb-2" />
-              <span className="text-zinc-200 text-xs font-bold uppercase">
+              <ListPlus className="text-indigo-700 mb-2" />
+              <span className="text-indigo-700 text-xs font-bold uppercase">
                 Add Song
               </span>
             </NavLink>
