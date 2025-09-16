a0c2ed62a2414824e9031313496887ba38738040
Horacio Dominguez <hord17@gmail.com>
2025-09-15 23:33:42 -0300

    change new song to local store

--- src/pages/NewSong.tsx
+++ src/pages/NewSong.tsx
@@ -1,15 +1,18 @@
 import { SongForm } from "@/modules/songs/components/form/SongForm"
 import type { Song } from "@/modules/songs/types/song.types"
+import { storage } from "@/services/storage"
 
 type Props = {
   songs: Song[]
   setSongs: React.Dispatch<React.SetStateAction<Song[]>>
 }
 
-export default function NewSong({ songs, setSongs }: Props) {
-  const handleAddSong = (song: Song) => {
-    const newSongs = [...songs, song]
-    setSongs(newSongs)
+export default function NewSong({ setSongs }: Props) {
+  const handleAddSong = async (song: Song) => {
+    await storage.saveSong(song)
+
+    const dbSongs = await storage.getSongs()
+    setSongs(dbSongs)
   }
 
   return (
